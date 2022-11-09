d6fe0a94784ce568c875d4c3b60ad2b99e92e91e
Daniel Apostoli <danielgapostoli@gmail.com>
2022-11-09 11:33:33 -0600

    fixed font issue

--- meme.py
+++ meme.py
@@ -25,10 +25,10 @@ def generate_meme(path=None, body=None, author=None):
 
     if body is None:
         quote_files = [
-            # './_data/DogQuotes/DogQuotesTXT.txt',
+            './_data/DogQuotes/DogQuotesTXT.txt',
             './_data/DogQuotes/DogQuotesDOCX.docx',
-            # './_data/DogQuotes/DogQuotesPDF.pdf',
-            # './_data/DogQuotes/DogQuotesCSV.csv'
+            './_data/DogQuotes/DogQuotesPDF.pdf',
+            './_data/DogQuotes/DogQuotesCSV.csv'
             ]
         quotes = []
         for f in quote_files:
